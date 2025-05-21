bf976a2781546c72dc979e43614900dc1c85a560
M Awais Ch <awaisfaryad25@gmail.com>
2025-05-21 14:57:02 +0500

    code format

--- src/components/global/PaginationControls.jsx
+++ src/components/global/PaginationControls.jsx
@@ -10,7 +10,8 @@ const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNe
   return (
     <div className="p-[2px] flex justify-center items-center flex-wrap w-full max-w-xs mx-auto rounded-full gap-2 p- text-white bg-[var(--bgSecondary)] border border-[var(--border)] shadow-md">
       {/* Start Button */}
-      <button onClick={goToStart} disabled={currentPage === 1}
+      <button 
+        onClick={goToStart} disabled={currentPage === 1}
         className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
         <MdKeyboardDoubleArrowLeft/>
@@ -18,8 +19,7 @@ const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNe
 
       {/* Previous Button */}
       <button
-        onClick={onPrev}
-        disabled={currentPage === 1}
+        onClick={onPrev} disabled={currentPage === 1}
         className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
         <MdKeyboardArrowLeft/>
@@ -28,11 +28,9 @@ const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNe
       {/* Page Numbers */}
       {Array.from({ length: totalPages }, (_, i) => (
         <button
-          key={i + 1}
-          onClick={() => onPageChange(i + 1)}
-          className={`px-[8px] py-[2px] border text-sm rounded-full border-[var(--border)] ${
-            currentPage === i + 1 ? 'bg-[var(--primary)] text-black font-bold' : 'hover:bg-[var(--secondary)]'
-          }`}
+          key={i + 1} onClick={() => onPageChange(i + 1)}
+          className={`px-[8px] py-[2px] border text-sm rounded-full border-[var(--border)] 
+            ${currentPage === i + 1 ? 'bg-[var(--primary)] text-black font-bold' : 'hover:bg-[var(--secondary)]'}`}
         >
           {i + 1}
         </button>
@@ -40,15 +38,15 @@ const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNe
 
       {/* Next Button */}
       <button
-        onClick={onNext}
-        disabled={currentPage === totalPages}
+        onClick={onNext} disabled={currentPage === totalPages}
         className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
         <MdKeyboardArrowRight/>
       </button>
 
       {/* End Button */}
-      <button onClick={goToEnd} disabled={currentPage === totalPages}
+      <button 
+        onClick={goToEnd} disabled={currentPage === totalPages}
         className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
         <MdKeyboardDoubleArrowRight/>
