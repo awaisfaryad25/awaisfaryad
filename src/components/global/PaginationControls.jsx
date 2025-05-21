412fa94c7263ad0768e56a7fd15f3cad930b6e52
M Awais Ch <awaisfaryad25@gmail.com>
2025-05-21 14:53:57 +0500

    pagination controlar design

--- src/components/global/PaginationControls.jsx
+++ src/components/global/PaginationControls.jsx
@@ -1,27 +1,28 @@
 
+import { MdKeyboardDoubleArrowLeft, MdKeyboardArrowLeft, MdKeyboardArrowRight, MdKeyboardDoubleArrowRight  } from "react-icons/md";
+
+
 const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNext, }) => {
   
   const goToStart = () => onPageChange(1);
   const goToEnd = () => onPageChange(totalPages);
 
   return (
-    <div className="flex justify-center items-center flex-wrap gap-2 pt-6 text-white">
+    <div className="p-[2px] flex justify-center items-center flex-wrap w-full max-w-xs mx-auto rounded-full gap-2 p- text-white bg-[var(--bgSecondary)] border border-[var(--border)] shadow-md">
       {/* Start Button */}
-      <button
-        onClick={goToStart}
-        disabled={currentPage === 1}
-        className="px-3 py-1 border rounded-md border-gray-600 hover:bg-gray-700 disabled:opacity-50"
+      <button onClick={goToStart} disabled={currentPage === 1}
+        className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
-        Start
+        <MdKeyboardDoubleArrowLeft/>
       </button>
 
       {/* Previous Button */}
       <button
         onClick={onPrev}
         disabled={currentPage === 1}
-        className="px-3 py-1 border rounded-md border-gray-600 hover:bg-gray-700 disabled:opacity-50"
+        className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
-        Prev
+        <MdKeyboardArrowLeft/>
       </button>
 
       {/* Page Numbers */}
@@ -29,8 +30,8 @@ const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNe
         <button
           key={i + 1}
           onClick={() => onPageChange(i + 1)}
-          className={`px-3 py-1 border rounded-md border-gray-600 ${
-            currentPage === i + 1 ? 'bg-[var(--primary)] text-black font-bold' : 'hover:bg-gray-700'
+          className={`px-[8px] py-[2px] border text-sm rounded-full border-[var(--border)] ${
+            currentPage === i + 1 ? 'bg-[var(--primary)] text-black font-bold' : 'hover:bg-[var(--secondary)]'
           }`}
         >
           {i + 1}
@@ -41,18 +42,16 @@ const PaginationControls = ({currentPage, totalPages, onPageChange, onPrev, onNe
       <button
         onClick={onNext}
         disabled={currentPage === totalPages}
-        className="px-3 py-1 border rounded-md border-gray-600 hover:bg-gray-700 disabled:opacity-50"
+        className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
-        Next
+        <MdKeyboardArrowRight/>
       </button>
 
       {/* End Button */}
-      <button
-        onClick={goToEnd}
-        disabled={currentPage === totalPages}
-        className="px-3 py-1 border rounded-md border-gray-600 hover:bg-gray-700 disabled:opacity-50"
+      <button onClick={goToEnd} disabled={currentPage === totalPages}
+        className="text-3xl text-[var(--primary)] disabled:opacity-50"
       >
-        End
+        <MdKeyboardDoubleArrowRight/>
       </button>
     </div>
   );
