5a158149e5d20fe1c7e2c5647b75d7ba70a0a6bb
M Awais Ch <awaisfaryad25@gmail.com>
2025-06-04 16:35:07 +0500

    resume projects added

--- src/components/resume/Projects.jsx
+++ src/components/resume/Projects.jsx
@@ -3,42 +3,42 @@ import { AiFillFolderOpen } from 'react-icons/ai';
 
 const projects = [
   {
-    icon: <FaProjectDiagram className="text-[var(--primary)]" />,
+    icon: <FaProjectDiagram />,
     title: 'Trade Today',
     description: 'AI-powered crypto insights platform',
     link: 'https://tradetoday.io/',
     details: 'Engineered TradeToday, an AI-driven web app delivering real-time crypto market insights to empower users with smarter trading opportunities.',
   },
   {
-    icon: <AiFillFolderOpen className="text-[var(--primary)]" />,
+    icon: <AiFillFolderOpen />,
     title: 'Code Miner',
     description: 'Official company website',
     link: 'https://codeminer.co/',
     details: 'Designed and developed official website for Code Miner, the company I work for, highlighting its software services, projects, and providing an interface for client communication.',
   },
   {
-    icon: <AiFillFolderOpen className="text-[var(--primary)]" />,
+    icon: <AiFillFolderOpen />,
     title: 'GHS&E',
     description: 'Greenhouse Solar and Electrical',
     link: 'https://ghse.com.au/',
     details: 'Built a web app for GHS%E, a solar installation company, with service listings, project showcases, inquiry forms, and an admin panel for managing services and client requests.',
   },
   {
-    icon: <AiFillFolderOpen className="text-[var(--primary)]" />,
+    icon: <AiFillFolderOpen />,
     title: 'Helper Log',
     description: 'Super Admin Panel',
     link: 'https://helperlog.helpoutweb.com/',
     details: 'Contributed to a shipment management dashboard, implementing Manifest, PO, and Invoice management, along with Agent/Runner modules and secure Super Admin',
   },
   {
-    icon: <AiFillFolderOpen className="text-[var(--primary)]" />,
+    icon: <AiFillFolderOpen />,
     title: 'GPP',
     description: 'GhoomoPhiroo Pakistan',
     link: 'http://www.ghoomophiroo.com/',
     details: 'Built a user-friendly tourist web application for exploring and booking travel experiences across Pakistan, featuring trip bookings, lucky draw systems, destination management, and an admin panel for seamless backend operations.',
   },
   {
-    icon: <AiFillFolderOpen className="text-[var(--primary)]" />,
+    icon: <AiFillFolderOpen />,
     title: 'Finarch',
     description: 'Architectural Web App',
     link: 'http://www.finarch.co',
@@ -54,20 +54,18 @@ const Projects = () => {
       <ol className="relative border-s border-[var(--border)] dark:border-gray-70">
         {projects.map((project, index) => (
           <li key={index} className="mb-10 ms-6">
-            <span className="absolute flex items-center justify-center size-8 bg-[var(--border)] rounded-full -start-4">
+            <span className="absolute flex items-center justify-center size-8 text-[var(--primary)] bg-[var(--border)] rounded-full -start-4">
               {project.icon}
             </span>
             <div className="flex items-center gap-3">
               <h3 className="text-lg font-semibold">{project.title}</h3>
-              <a href={project.link} target="_blank" rel="noopener noreferrer"
-                className="text-blue-500 text-sm underline"
-              >
+              <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm underline">
                 <FaLink />
               </a>
             </div>
             <p className="text-sm mb-1">{project.description}</p>
             {project.details && (
-              <ul>
+              <ul className='text- '>
                 <li>{project.details}</li>
               </ul>
             )}
