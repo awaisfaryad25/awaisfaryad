f771ce29c8e64776d2aa37033f9acf517e7c9ecf
M Awais Ch <awaisfaryad25@gmail.com>
2025-06-04 13:28:27 +0500

    experience added

--- src/components/resume/Experience.jsx
+++ src/components/resume/Experience.jsx
@@ -1,48 +1,46 @@
 import { FaBriefcase, FaLaptopCode } from 'react-icons/fa';
-import { MdLaptopMac } from 'react-icons/md';        // Mac-style laptop icon
-// import { RiLaptopLine } from 'react-icons/ri';   
-import { BiLaptop } from 'react-icons/bi';            // Basic laptop shape
 
 const Experience = () => {
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-2">Professional Experience</h2>
       <hr className="mb-3" />
-      <ol className="relative border-s border-[var(--secondary)] dark:border-gray-70">
+      <ol className="relative border-s border-[var(--border)] dark:border-gray-70">
         {/* Job 1 - Associate Software Developer */}
         <li className="mb-10 ms-6">
-          <span className="absolute flex items-center justify-center size-8 bg-[var(--secondary)] rounded-full -start-4 ">
-            <FaLaptopCode  className='text-[var(--primary)]'/>
+          <span className="absolute flex items-center justify-center size-8 bg-[var(--border)] rounded-full -start-4 ">
+            <FaBriefcase  className='text-[var(--primary)]'/>
           </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-            Associate Software Developer
-          </h3>
+          <div className="flex items-center justify-between">
+            <h3 className="mb-1 text-lg font-semibold">Associate Software Developer</h3>
+            <h4 className='text-sm'>Sep 2024 – Present</h4>
+          </div>
           <p className="block mb-2 text-sm font-normal leading-none">
-            Sep 2024 – Present · CodeMiners IT & Consultancy · Lahore, Pakistan
+            CodeMiners IT & Consultancy · Lahore, Pakistan
           </p>
           <ul className="list-disc list-inside text-base font-normal text-gray-500 dark:text-gray-400">
-            <li>Work independently and in team to handle client-side logic.</li>
-            <li>Develop responsive and dynamic web applications across devices.</li>
+            <li>Worked independently on client-side development tasks and collaborated effectively within a dynamic team.</li>
+            <li>Handled front-end logic, UI implementation, and ensured seamless user experience across projects.</li>
+            <li>Contributed to the successful delivery of web applications by writing clean, maintainable, and efficient code.</li>
+            <li>Integrated and consumed REST APIs & WebSockets to handle dynamic data and enhance application functionality.</li>
           </ul>
         </li>
 
         {/* Job 2 - Intern */}
         <li className="mb-10 ms-6">
-        <span className="absolute flex items-center justify-center size-8 bg-[var(--secondary)] rounded-full -start-4 ">
-            <FaLaptopCode  className='text-[var(--primary)]'/>
+          <span className="absolute flex items-center justify-center size-8 bg-[var(--border)] rounded-full -start-4 ">
+            <FaBriefcase  className='text-[var(--primary)]'/>
           </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-            Frontend Developer Intern
-          </h3>
-          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            Nov 2023 – Apr 2024 · Alpha Rages · Lahore, Pakistan
-          </time>
+          <div className="flex items-center justify-between">
+            <h3 className="mb-1 text-lg font-semibold ">Frontend Developer Intern</h3>
+            <h4 className='text-sm'>Nov 2023 – Apr 2024</h4>
+          </div>
+          <p className="block mb-2 text-sm font-normal leading-none">Alpha Rages · Lahore, Pakistan</p>
           <ul className="list-disc list-inside text-base font-normal text-gray-500 dark:text-gray-400">
-            <li>Develop dynamic and interactive templates with HTML, CSS, JavaScript.</li>
-            <li>Collaborated with design and marketing teams.</li>
+            <li>Utilized HTML, CSS, and JavaScript to develop dynamic and interactive templates</li>
+            <li>Collaborate with design and marketing teams to implement responsive and userfriendly interfaces.</li>
           </ul>
         </li>
-
       </ol>
     </div>
   );
