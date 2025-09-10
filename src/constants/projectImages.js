cefa82377bb76ca207ff420815df3096a3b79c25
M Awais Ch <142403036+awaisfaryad25@users.noreply.github.com>
2025-09-10 20:01:42 +0500

    other images code remove

--- src/constants/projectImages.js
+++ src/constants/projectImages.js
@@ -1,31 +1,10 @@
 // src/assets/projectImages.js
 // InspiredEdible Images
-import InspiredEdible from '../assets/inspiredEdible/inspireEdibles.png';
-import BlogMode from '../assets/inspiredEdible/BlogMode.png';
-import BoomerMode from '../assets/inspiredEdible/BoomerMode.png';
-import WebRings from '../assets/inspiredEdible/web-rings.png';
-import CrtRed from '../assets/inspiredEdible/crt-red.png';
-import CrtGreen from '../assets/inspiredEdible/crt-green.png';
-import Admin from '../assets/inspiredEdible/admin.png';
-import AdminBlog from '../assets/inspiredEdible/adminBlog.png';
-import EditBlog from '../assets/inspiredEdible/editBlog.png';
-import AddSocial from '../assets/inspiredEdible/addSocial.png';
 
 // TradeToday Images
-import TradeToday from '../assets/tradeToday/tradetoday.png';
-import TradeLanding from '../assets/tradeToday/tradeLandingPage.png';
-import TradeAdminHome from '../assets/tradeToday/tradeAdminHome.png';
-import TradeAdminMarket from '../assets/tradeToday/tradeAdminMarket.png';
-import TradeAdminPredict from '../assets/tradeToday/tradeAdminPredict.png';
 
 // Other Project Images
-import CodeMiner from '../assets/projects/codeminer.png';
-import GPP from '../assets/projects/gpp.png';
-import Helperlog from '../assets/projects/helperlog.png';
-import GHSE from '../assets/projects/ghse.png';
-import Finarch from '../assets/projects/finarch.png';
-import Branddekho from '../assets/projects/branddekho.png';
-import GPS from '../assets/projects/gps.png';
+
 import Rungley from '../assets/projects/rungley.png';
 
 export const projectImages = {
